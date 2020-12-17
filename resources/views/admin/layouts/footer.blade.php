a0b25698664f1164eb56418d3c29078816971352
kalam ahmed <kalamahmed714@gmail.com>
2020-12-17 14:10:43 +0600

    admin template setup

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+      <div class="copyright text-center my-auto">
+        <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
+          <b><a href="https://kalam714.pythonanywhere.com/" target="_blank">Kalam Ahmed</a></b>
+        </span>
+      </div>
+    </div>
+  </footer>
+  <!-- Footer -->
+</div>
+</div>
+
+
+
+<script src="{{asset('backend/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('backend/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+<script src="{{asset('backend/js/ruang-admin.min.js')}}"></script>
+<script src="{{asset('backend/vendor/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('backend/js/demo/chart-area-demo.js')}}"></script>  
+</body>
+
+</html>
